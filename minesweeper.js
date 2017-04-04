bb2cc6a29a49d569c0cafd5d919a7b75b2888b39
Sam Boyarsky <sam.boyarsky@theironyard.com>
2017-04-04 17:18:52 -0400

    stubbed out functions

---
+++ minesweeper.js
@@ -0,0 +1,68 @@
+let X = 10;
+let Y = 10;
+
+function clickCell(e) {
+    // Implement this.
+}
+
+function rightClickCell(e) {
+    // Implement this.
+}
+
+function countNeighboringMines() {
+    // Implement this.
+}
+
+
+function getCell(x, y){
+    return $("#cell_" + x + "_" + y);
+}
+
+function getNeighbors(currentCell){
+    // x-1,y-1   x,y-1   x+1,y-1
+    // x-1,y     x,y     x+1,y
+    // x-1,y+1   x,y+1   x+1, y+1
+    let y = currentCell.parentNode.rowIndex;
+    let x = currentCell.cellIndex;
+    let xe = x-1;
+    let xw = x+1;
+    let yn = y-1;
+    let ys = y+1;
+    let ne = getCell(xe, yn);
+    let n  = getCell(x,  yn);
+    let nw = getCell(xw, yn);
+    let e  = getCell(xe, y);
+    let w  = getCell(xw,  y);
+    let se = getCell(xe, ys);
+    let s  = getCell(x,  ys);
+    let sw = getCell(xw, ys);
+
+    return [ne,n,nw,e,w,se,s,sw];
+}
+
+
+function buildGameBoard(x, y) {
+    let $board = $('<table>');
+    for(var i = 0; i < y; i++) {
+        let $row = $('<tr>');
+
+        $board.append($row);
+        for(var j = 0; j < x; j++) {
+            let $cell = $('<td>');
+            $row.append($cell);
+            if(Math.random() < .2) {
+                $cell.addClass('mine')
+            }
+            $cell.attr('id', 'cell_' + j + '_' + i);
+            $cell.on('click', clickCell);
+            $cell.on('contextmenu', rightClickCell);
+        }
+    }
+    $('#gameBoard').append($board);
+}
+
+function startGame(){
+    buildGameBoard(X, Y);
+}
+
+$("#startButton").click(startGame)
